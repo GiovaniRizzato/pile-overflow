8009f47eb37082e6e1db6ee52e6f49782b2caa51
Giovani Rizzato <giovanirizzato@gmail.com>
2024-05-18 21:02:03 -0300

    separating the project into multiple

---
+++ tree-elo-backend/src/main/java/br/com/giovanirizzato/treeelo/TreeEloApplication.java
@@ -0,0 +1,13 @@
+package br.com.giovanirizzato.treeelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeEloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeEloApplication.class, args);
+	}
+
+}
---
+++ tree-elo-backend/src/test/java/br/com/giovanirizzato/treeelo/TreeEloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanirizzato.treeelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeEloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
